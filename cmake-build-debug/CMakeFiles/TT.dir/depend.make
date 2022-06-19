0cd1e9775750176392cbf5ccafa53f8b4e19a063
B1GGersnow <2642448440@qq.com>
2022-06-19 16:03:59 +0800

    123

--- cmake-build-debug/CMakeFiles/TT.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for TT.
-# This may be replaced when dependencies are built.
--- hnie/12.1/A.cpp
+++
@@ -1,9 +0,0 @@
-#include "bits/stdc++.h"
-
-#define fore(i, l, r) for(int i = l;i <= r;i++)
-using namespace std;
-
-int main() {
-
-    return 0;
-}
